148204edaa7621179362efa24235f8da9143a6ca
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-03-06 03:41:31 +0530

    Update User.js

--- backend/User.js
+++ backend/User.js
@@ -1,19 +1,33 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+
   name:{
     type:String,
     required:true
   },
+
   email:{
     type:String,
     required:true,
-    unique:true
+    unique:true,
+    match:[/^\S+@\S+\.\S+$/, "Please use a valid email address"]
   },
+
   password:{
     type:String,
     required:true
+  },
+
+  verified:{
+    type:Boolean,
+    default:false
+  },
+
+  verificationToken:{
+    type:String
   }
+
 });
 
 module.exports = mongoose.model("User", userSchema);
